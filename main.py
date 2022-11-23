257127fb0be8110886890e4161581678d7bf96c4
EdgarAraya <44482655+EdgarAraya@users.noreply.github.com>
2022-11-23 13:30:23 -0300

    testing

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import statistics
 import json
 from numpy import mean
 import  Bio.PDB.Residue as Residue
-#path = '6ZE1_1b825_unrelaxed_rank_1_model_3.pdb' # your file path here
-path= "NEGATIVE_09_fcd4d_unrelaxed_rank_1_model_3.pdb" #negative
+path = '6ZE1_1b825_unrelaxed_rank_1_model_3.pdb' # your file path here
+#path= "NEGATIVE_09_fcd4d_unrelaxed_rank_1_model_3.pdb" #negative
+#path= "NEGATIVE_05_462ca_unrelaxed_rank_1_model_3.pdb"
 #path= '6WX1_4897e_unrelaxed_rank_1_model_1.pdb'
+#path='7FDO_5ce41_unrelaxed_rank_1_model_2.pdb'
 json_file="6ZE1_1b825_unrelaxed_rank_1_model_3_scores.json"
 p = Bio.PDB.PDBParser()
 structure = p.get_structure('myStructureName', path)
@@ -120,17 +122,18 @@ for residue1 in chain:
                 distance = residue1['CA'] - residue2['CA']
             except KeyError:
                 continue
-            if distance < 10:
+            if distance < 8:
                 residue_in_interface=True
                 if residue1.get_id()[1] not in index_chain1:
                      index_chain1.append(residue1.get_id()[1])
                      for atom in residue1.get_atoms():
                         new_bfactors.append(atom.get_bfactor()*-1)
+                        atom.set_bfactor(atom.get_bfactor()*-1)
                 if residue2.get_id()[1] not in index_chain2:
                     index_chain2.append(residue2.get_id()[1])
 
                 for atom in residue1.get_atoms():
-                    new_bfactors.append(atom.get_bfactor()*-1)
+                    new_bfactors.append(atom.get_bfactor()*-100)
 
                 
                 print(residue1, residue2, distance)
@@ -138,8 +141,33 @@ for residue1 in chain:
         #break
 
 #print(new_bfactors)
-print(bfactors)
-print(len(bfactors))
-print(len(new_bfactors))
-print(len(index_chain1))
-print(index_chain1)
+#print(bfactors)
+#print(len(bfactors))
+#print(len(new_bfactors))
+#print(len(index_chain1))
+#print(index_chain1)
+
+
+print("average plDDT before: ",mean(bfactors))
+
+bfactors2= [a.get_bfactor() for a in structure.get_atoms()]
+bfactor_test=[]
+#print(bfactors2)
+for factor in bfactors2:
+    if factor<0:
+        factor=factor*-0.75
+    else:
+        factor=factor*0.25
+    bfactor_test.append(factor)
+
+print(bfactor_test)
+print("average XplDDT after: ",mean(bfactor_test))
+
+print("Interaction?")
+if (mean(bfactor_test)>24):
+    print("Probably Yes")
+else:
+    print("Probably No")
+
+
+
